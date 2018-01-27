28d3c36549337e4430ace9fabaeaaafaf46489c1
ArtixBot <williamwongcastle@gmail.com>
2018-01-27 15:59:20 -0500

    why is AI so difficult

--- CoMA/Assets/Resources/Scripts/AIMovement.cs
+++ CoMA/Assets/Resources/Scripts/AIMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class AIMovement : BaseMovement {
 
 	public float time = 0.0f;
-	private Vector2 targetPos = Vector2.zero;
+	private Vector3 targetPos = Vector3.zero;
 	public enum AIState { Idle, Talk, Seek, Enforce };
 	public AIState state;
 
@@ -40,26 +40,28 @@ public class AIMovement : BaseMovement {
 			updateAI ();
 		}
 
-		transform.position = Vector2.MoveTowards(transform.position, targetPos, 0.15f);
+		transform.position = Vector2.MoveTowards(transform.position, targetPos, 0.10f);
 	}
 
 	void updateAI(){
-		Vector2 nextTarget = transform.position;
 		switch (state) { 
 		case AIState.Idle:
 			// No movement. Do nothing. Default state.
-			if (transform.position.x == targetPos.x && transform.position.z == targetPos.y) {
-				// Currently at target position. Idle for 2.5 - 5 seconds, then re-enter seek mode.
-				time = Random.Range (2.5f, 5.0f);
+			if (state == AIState.Idle && transform.position == targetPos) {
+				time = Random.Range (1.5f, 3.0f);
 				state = AIState.Seek;
 			}
 			break;
 		case AIState.Seek:
 			// By default: search for nearby person. If low mood: search for nearby happy person.
 			// Else: go randomly.
-			targetPos.x = (transform.position.x - Random.Range (-5.0f, 5.0f));
-			targetPos.y = (transform.position.z - Random.Range (-5.0f, 5.0f));
-			time = Random.Range (2.5f, 5.0f);
+
+			// Case 1: Mood is not low. Search for a nearby person.
+			// Case 2: Mood is low. Search for nearby happy person.
+			// Case 3: Nobody is nearby. Move within random location.
+			targetPos.x = (transform.position.x - Random.Range (-1.5f, 1.5f));
+			targetPos.y = (transform.position.z - Random.Range (-1.5f, 1.5f));
+			state = AIState.Idle;
 			break;
 		case AIState.Talk:
 			// Placeholder for now.
