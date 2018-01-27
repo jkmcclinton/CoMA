7872f4035cd0c6a0a11bf5bb2ed40bab81a4b458
ArtixBot <williamwongcastle@gmail.com>
2018-01-26 23:44:24 -0500

    Elevator script mostly done

---
+++ CoMA/Assets/Resources/Scripts/ElevatorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour {
+
+	private bool touchingElevator = false;
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		// Player activates elevator.
+		if ((Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && touchingElevator) {
+			Debug.Log ("Moving up!");
+			anim.SetTrigger ("Elevator");
+			TransportPlayer ();
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			touchingElevator = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Player") {
+			touchingElevator = false;
+		}
+	}
+
+	void TransportPlayer() {
+		GameObject.FindGameObjectWithTag ("Player").transform.position;
+	}
+}
