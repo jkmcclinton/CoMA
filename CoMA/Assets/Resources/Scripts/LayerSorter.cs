414337b914b729dde2a19e1c0d78cf2bf7729c86
Ryan Kann <rak3me@virginia.edu>
2018-01-27 20:42:39 -0500

    Started Music

---
+++ CoMA/Assets/Resources/Scripts/LayerSorter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorter : MonoBehaviour {
+
+//	public static int instance = 0;
+//
+//	void Start () {
+//		if (instance == 0) {
+//			CreateSortingLayers ();
+//		}
+//		instance++;
+//	}
+//
+//	void CreateSortingLayers() {
+//		//SortingLayer.
+//	}
+
+	public static int instance = 0;
+
+	private int offset;
+	private int baseSortingOrder;
+
+	// Use this for initialization
+//	void Start () {
+//		if (tag != "Player") {
+//			instance++;
+//			foreach (Transform child in transform) {
+//				child.GetComponent<SpriteRenderer> ().sortingOrder -= 100 + 5 * instance;
+//			}
+//			baseSortingOrder = transform.GetChild (0).GetComponent<SpriteRenderer> ().sortingOrder;
+//		} else {
+//			foreach (Transform child in transform) {
+//				child.GetComponent<SpriteRenderer> ().sortingOrder -= 10;
+//			}
+//			baseSortingOrder = transform.GetChild (0).GetComponent<SpriteRenderer> ().sortingOrder;
+//		}
+//	}
+
+	void Update () {
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).GetComponent<SpriteRenderer> ().sortingOrder = (-10 * Mathf.RoundToInt (200 * transform.position.y) - i);
+		}
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//		if (transform.position.y < -1.9f) {
+//			offset = 0;
+//		} else if (transform.position.y < 0.1f) {
+//			offset = 1;
+//		} else if (transform.position.y < 2.1f) {
+//			offset = 2;
+//		} else {
+//			offset = 3;
+//		}
+//
+//		//GetComponent<SpriteRenderer> ().sortingOrder = baseSortingOrder - offset * 200;
+//		for (int i = 0; i < transform.childCount; i++) {
+//			transform.GetChild (i).GetComponent<SpriteRenderer> ().sortingOrder = baseSortingOrder - i - offset * 200;
+//		}
+//	}
+}
---
+++ CoMA/Assets/Resources/Scripts/MusicCrossfade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicCrossfade : MonoBehaviour {
+
+	[Range(-10, 10)] public int happyMeter = 0;
+	public AudioMixer mixer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (happyMeter < -3) {
+			mixer.SetFloat ("NAudioVol", -80f);
+			mixer.SetFloat ("NDrumsVol", -80f);
+			mixer.SetFloat ("DAudioVol", 0f);
+			mixer.SetFloat ("DDrumsVol", 0f);
+			mixer.SetFloat ("HAudioVol", -80f);
+			mixer.SetFloat ("HDrumsVol", -80f);
+		} else if (happyMeter < 4) {
+			mixer.SetFloat ("NAudioVol", 1f);
+			mixer.SetFloat ("NDrumsVol", 1f);
+			mixer.SetFloat ("DAudioVol", -80f);
+			mixer.SetFloat ("DDrumsVol", -80f);
+			mixer.SetFloat ("HAudioVol", -80f);
+			mixer.SetFloat ("HDrumsVol", -80f);
+		} else {
+			mixer.SetFloat ("NAudioVol", -80f);
+			mixer.SetFloat ("NDrumsVol", -80f);
+			mixer.SetFloat ("DAudioVol", -80f);
+			mixer.SetFloat ("DDrumsVol", -80f);
+			mixer.SetFloat ("HAudioVol", 1f);
+			mixer.SetFloat ("HDrumsVol", 1f);
+		}
+	}
+}
